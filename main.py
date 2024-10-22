79286e08c2ed2b9437e4850fa730e9d4e4b40ac5
Rodrigo Carvalho Nogueira júnior <rodrigosms103042r6@gmail.com>
2024-10-22 12:56:44 -0300

    installing tailwind and using templates

--- main.py
+++ main.py
@@ -10,10 +10,28 @@ def main():
     return render_template('index.html')
 
 @app.errorhandler(404)
-def page_not_found(e):
+def page_not_found(error):
     return (
-        render_template('404.html'), e==404
+        render_template('404.html'), error == 404
         )
+
+@app.errorhandler(500)
+def server_error(error):
+    return (
+        render_template('500.html') , error == 500 
+    )
+
+@app.route('/about')
+def about():
+    
+    return render_template('about.html')
+
+@app.route('/user/<username>')
+def user(username):
+    return (
+        render_template('user.html', name = username)
+    )
         
 if __name__ == '__main__':
-    app.run( debug=True)
+
+    app.run( debug=True , port = 3000)
